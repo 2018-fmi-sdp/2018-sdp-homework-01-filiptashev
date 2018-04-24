db571f85289317f77dcba19a8394ad326a12211c
filiptashev <37833855+filiptashev@users.noreply.github.com>
2018-04-24 22:18:28 +0300

    Update homework 2problem.cpp

--- homework 2problem.cpp
+++ homework 2problem.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 #include <cstring>
-#include <cstdlib>
+
 
 using namespace std;
 
@@ -15,28 +15,26 @@ int main()
     while(true) {
 a=1;
         cin.getline(line, 1000);
-        if (strcmp(line, "quit") == 0) {
-            break;
-        }
         if (strncmp(line, "fn11245.info", strlen("fn11245.info")) == 0) {a=0;
- cout<<"Filip Tashev Matematika 2017";
+ cout<<"Filip Tashev Matematika 2017"<<endl;
         }
         if (strncmp(line, "fn11245.sdp.marks", strlen("fn11245.sdp.marks")) == 0){a=0;
              cout<<"TKsem="<<TKsem<<endl<<"Kt01="<<Kt01<<endl<<"Kt02="<<Kt02<<endl<<"Kz01="<<Kz01<<endl<<"Kz02="<<Kz02<<endl<<"TK="<<((TKsem+Kt01+Kt02+Kz01+Kz02)/5)<<endl;
     cout<<"It="<<It<<" Can skip? ";if(Kt01>=3.50&&Kt02>=3.50&&Kt01+Kt02>=9){cout<<"yes"<<(Kt01+Kt02)/2;}else{cout<<"no";};
-     cout<<"Iz="<<Iz<<" Can skip? ";if(Kz01>=3.50&&Kz02>=3.50&&Kz01+Kz02>=9){cout<<"yes"<<(Kz01+Kz02)/2;}else{cout<<"no";};
-    cout<<"I="<<((Iz+It)/2);cout<<" Skip T? " ;if(Kt01>=3.50&&Kt02>=3.50&&Kt01+Kt02>=9){cout<<" yes "<<((Kt01+Kt02)/2);}else{cout<<" no ";} cout<<" Skip Z? ";if(Kz01>=3.50&&Kz02>=3.50&&Kz01+Kz02>=9){cout<<" yes "<<(Kz01+Kz02)/2;}else{cout<<" no ";}; cout<<" Skip Both? ";
+     cout<<" Iz="<<Iz<<" Can skip? ";if(Kz01>=3.50&&Kz02>=3.50&&Kz01+Kz02>=9){cout<<"yes"<<(Kz01+Kz02)/2;}else{cout<<"no";};
+    cout<<" I="<<((Iz+It)/2);cout<<" Skip T? " ;if(Kt01>=3.50&&Kt02>=3.50&&Kt01+Kt02>=9){cout<<" yes "<<((Kt01+Kt02)/2);}else{cout<<" no ";} cout<<" Skip Z? ";if(Kz01>=3.50&&Kz02>=3.50&&Kz01+Kz02>=9){cout<<" yes "<<(Kz01+Kz02)/2;}else{cout<<" no ";}; cout<<" Skip Both? ";
     if(Kt01>=3.50&&Kt02>=3.50&&Kt01+Kt02>=9&&Kz01>=3.50&&Kz02>=3.50&&Kz01+Kz02>=9){cout<<((Kt01+Kt02+Kz01+Kz02)/4);}
     cout<<endl<<" O= "<<(2*(TKsem+Kt01+Kt02+Kz01+Kz02))/15+(Iz+It)/6;cout<<" Skip T ?";if(Kt01>=3.50&&Kt02>=3.50&&Kt01+Kt02>=9){cout<<" yes "<<((Kt01+Kt02)/2);}else{cout<<" no ";} cout<<" Skip Z? ";if(Kz01>=3.50&&Kz02>=3.50&&Kz01+Kz02>=9){cout<<" yes " <<((Kz01+Kz02)/2);}else{cout<<" no ";}; cout<<" Skip Both? ";
     if(Kt01>=3.50&&Kt02>=3.50&&Kt01+Kt02>=9&&Kz01>=3.50&&Kz02>=3.50&&Kz01+Kz02>=9){cout<<((Kt01+Kt02+Kz01+Kz02)/4);}
     cout<<endl<<"Skip with Average?";if(SemAttendance>80&&PracticeAttendance>80&&PracticePass==true){cout<<" yes ";}else{cout<<" no ";}
+    cout<<endl;
 ;}
 
 
 
         if (strncmp(line, "fn11245.sdp.info", strlen("fn11245.sdp.info")) == 0){a=0;
             cout<<"O=2/3*TK+1/3*I"<<endl<<"TK=1/5(Kt01+Kt02+Kz01+Kz02+TKsem)"<<endl<<"I=1/2*(It+Iz)"<<endl<<"Oswobovdawane ot kontrolni teoriq i zadaci e vyzmovno pri sredna ocenka nad 4,50 i dvete ocenki nad 3.50.Osvobovdawaneto stava sys srednata ocenka;"<<endl<<"Wyzmovno e i oswobovdawane pri80% prisystwie na seminaar i praktikum s Sem TK>5.00;Osvobovdawaneto stava s 3;";
-        }
+        ;cout<<endl;}
             if (strncmp(line, "fn11245.sdp.TKsem ", strlen("fn11245.sdp.TKsem ")) == 0)
                 {a=0;
         char* arguments = line + strlen("fn11245.sdp.TKsem ");
@@ -104,8 +102,8 @@ PracticePass=true;
 };
 
 
-if (a==1){cout<<"not comand";};
-        cout<<endl;
+if (a==1){cout<<"not comand"<<endl;};
     };
     return 0;
 }
+
